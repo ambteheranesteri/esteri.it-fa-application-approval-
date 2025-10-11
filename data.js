d3705ecca209f54f84be6ceb9d64844f4ea98b20
ambteheranesteri <ambteheran.esteri.it@tuta.io>
2025-10-11 05:10:09 +0330

    Update data.js

--- data.js
+++ data.js
@@ -1,38 +1,38 @@
-[
+const userData = [
   {
-    "username": "Mashal Saidy",
-    "password": "IT659349731",
-    "ceuNumber": "659349731",
-    "name": "Mashal",
-    "lastname": "Saidy",
-    "nationality": "Afghan",
-    "passportNumber": "p909090",
-    "nationalIDNumber": "p909090",
-    "gender": "Female",
-    "links": {
-      "finalResult": "https://ambteheranesteri.github.io/esteri.it-fa-application-approval-/SAIDY.pdf",
-      "uploadPassport": "https://example.com/mashal-passport",
-      "uploadDocuments": "https://example.com/mashal-docs",
-      "unhcrLetter": "https://example.com/mashal-unhcr",
-      "uploadFinger": "https://example.com/mashal-finger"
+    username: "Mashal Saidy",
+    password: "IT659349731",
+    ceuNumber: "659349731",
+    name: "Mashal",
+    lastname: "Saidy",
+    nationality: "Afghan",
+    passportNumber: "p909090",
+    nationalIDNumber: "p909090",
+    gender: "Female",
+    links: {
+      finalResult: "https://www.google.com",
+      uploadPassport: "https://example.com/mashal-passport",
+      uploadDocuments: "https://example.com/mashal-docs",
+      unhcrLetter: "https://example.com/mashal-unhcr",
+      uploadFinger: "https://example.com/mashal-finger"
     }
   },
   {
-    "username": "Ahmad Farhad",
-    "password": "AF123456789",
-    "ceuNumber": "123456789",
-    "name": "Ahmad",
-    "lastname": "Farhad",
-    "nationality": "Afghan",
-    "passportNumber": "p123456",
-    "nationalIDNumber": "n123456",
-    "gender": "Male",
-    "links": {
-      "finalResult": "https://ambteheranesteri.github.io/esteri.it-fa-application-approval-/sample2.pdf",
-      "uploadPassport": "https://example.com/ahmad-passport",
-      "uploadDocuments": "https://example.com/ahmad-docs",
-      "unhcrLetter": "https://example.com/ahmad-unhcr",
-      "uploadFinger": "https://example.com/ahmad-finger"
+    username: "MASHAL SAIDY",
+    password: "411914534",
+    ceuNumber: "CEU411914534",
+    name: "MASHAL",
+    lastname: "SAIDY",
+    nationality: "AFGHAN",
+    passportNumber: "",
+    nationalIDNumber: "00000",
+    gender: "female",
+    links: {
+      finalResult: "https://youtube.com",
+      uploadPassport: "https://example.com/mashal2-passport",
+      uploadDocuments: "https://example.com/mashal2-docs",
+      unhcrLetter: "https://example.com/mashal2-unhcr",
+      uploadFinger: "https://example.com/mashal2-finger"
     }
   }
-]
+];
