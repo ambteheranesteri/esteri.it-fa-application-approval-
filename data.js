5a1615c5d877d5762f098bacaddd1199f5f4a40a
ambteheranesteri <ambteheran.esteri.it@tuta.io>
2025-10-11 04:52:55 +0330

    Update data.js

--- data.js
+++ data.js
@@ -9,30 +9,6 @@ const userData = [
     passportNumber: "p909090",
     nationalIDNumber: "p909090",
     gender: "Female",
-    links: {
-      finalResult: "https://www.google.com",
-      uploadPassport: "https://example.com/mashal-passport",
-      uploadDocuments: "https://example.com/mashal-docs",
-      unhcrLetter: "https://example.com/mashal-unhcr",
-      uploadFinger: "https://example.com/mashal-finger"
-    }
-  },
-  {
-    username: "MASHAL SAIDY",
-    password: "411914534",
-    ceuNumber: "CEU411914534",
-    name: "MASHAL",
-    lastname: "SAIDY",
-    nationality: "AFGHAN",
-    passportNumber: "",
-    nationalIDNumber: "00000",
-    gender: "female",
-    links: {
-      finalResult: "https://youtube.com",
-      uploadPassport: "https://example.com/mashal2-passport",
-      uploadDocuments: "https://example.com/mashal2-docs",
-      unhcrLetter: "https://example.com/mashal2-unhcr",
-      uploadFinger: "https://example.com/mashal2-finger"
-    }
+    pdfLink: "https://ambteheranesteri.github.io/esteri.it-fa-application-approval-/pdfs/SAIDY.pdf"
   }
 ];
