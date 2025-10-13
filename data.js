491209063896debfa433f5562b470790c2814a00
ambteheranesteri <ambteheran.esteri.it@tuta.io>
2025-10-13 20:19:12 +0330

    Update data.js

--- data.js
+++ data.js
@@ -1,76 +1,74 @@
 const userData = [
-  {
-    username: "BIBI SAYEDA",
-    password: "IT457219308",
-    ceuNumber: "ITA/IRN/2025/457219308",
-    name: "BIBI SAYEDA",
-    lastname: "HUSSAIN KHAIL",
-    nationality: "AFGHAN",
-    passportNumber: "0700-04573",
-    nationalIDNumber: "0700-04573",
-    gender: "Female",
-    links: {
-      finalResult: "https://ambteheranesteri.github.io/approval-result-view/ITAIRN2025457219308.pdf",
-      uploadPassport: "https://example.com/mashal-passport",
-      uploadDocuments: "https://example.com/mashal-docs",
-      unhcrLetter: "https://example.com/mashal-unhcr",
-      uploadFinger: "https://example.com/mashal-finger"
-    }
-  },
-  {
-    username: "ABDUL GHAFOOR",
-    password: "IT659349731",
-    ceuNumber: "ITA/IRN/2025/659349731",
-    name: "ABDUL GHAFOOR",
-    lastname: "HAKIMI",
-    nationality: "AFGHAN",
-    passportNumber: "P00766839",
-    nationalIDNumber: "0600-20650",
-    gender: "male",
-    links: {
-      finalResult: "https://ambteheranesteri.github.io/approval-result-view/ITAIRN2025659349731.pdf",
-      uploadPassport: "https://example.com/mashal2-passport",
-      uploadDocuments: "https://example.com/mashal2-docs",
-      unhcrLetter: "https://example.com/mashal2-unhcr",
-      uploadFinger: "https://example.com/mashal2-finger"
-    }
-  },
-  {
-    username: "MASHAL SAIDY",
-    password: "IT761319690",
-    ceuNumber: "ITA/IRN/2025/761319690",
-    name: "MASHAL",
-    lastname: "SAIDY",
-    nationality: "AFGHAN",
-    passportNumber: "30811641",
-    nationalIDNumber: "30811641",
-    gender: "Female",
-    links: {
-      finalResult: "https://ambteheranesteri.github.io/approval-result-view/ITAIRN2025761319690.pdf",
-      uploadPassport: "https://example.com/farah-passport",
-      uploadDocuments: "https://example.com/farah-docs",
-      unhcrLetter: "https://example.com/farah-unhcr",
-      uploadFinger: "https://example.com/farah-finger"
-    }
-  },
-  // **شخص جدید اضافه شده**
-  {
-    username: "SAYED_JAMSHID",
-    password: "IT197531842",
-    ceuNumber: "64511169",
-    name: "SAYED JAMSHID",
-    lastname: "HUSSAINI",
-    nationality: "AFGHAN",
-    passportNumber: "P01550807",
-    nationalIDNumber: "1400-0101-12696",
-    gender: "Male",
-    links: {
-      finalResult: "https://ambteheranesteri.github.io/approval-result-view/ITTEH-2025-5601612473-appointment.pdf",
-      uploadPassport: "https://example.com/sayed-jamshid-passport",
-      uploadDocuments: ""https://ambteheranesteri.github.io/approval-result-view/ITTEH-2025-5601612473-appointment.pdf",",
-      unhcrLetter: "https://example.com/sayed-jamshid-unhcr",
-      uploadFinger: "https://example.com/sayed-jamshid-finger"
-    }
-  }
+  {
+    username: "BIBI SAYEDA",
+    password: "IT457219308",
+    ceuNumber: "ITA/IRN/2025/457219308",
+    name: "BIBI SAYEDA",
+    lastname: "HUSSAIN KHAIL",
+    nationality: "AFGHAN",
+    passportNumber: "0700-04573",
+    nationalIDNumber: "0700-04573",
+    gender: "Female",
+    links: {
+      finalResult: "https://ambteheranesteri.github.io/approval-result-view/ITAIRN2025457219308.pdf",
+      uploadPassport: "https://example.com/mashal-passport",
+      uploadDocuments: "https://example.com/mashal-docs",
+      unhcrLetter: "https://example.com/mashal-unhcr",
+      uploadFinger: "https://example.com/mashal-finger"
+    }
+  },
+  {
+    username: "ABDUL GHAFOOR",
+    password: "IT659349731",
+    ceuNumber: "ITA/IRN/2025/659349731",
+    name: "ABDUL GHAFOOR",
+    lastname: "HAKIMI",
+    nationality: "AFGHAN",
+    passportNumber: "P00766839",
+    nationalIDNumber: "0600-20650",
+    gender: "male",
+    links: {
+      finalResult: "https://ambteheranesteri.github.io/approval-result-view/ITAIRN2025659349731.pdf",
+      uploadPassport: "https://example.com/mashal2-passport",
+      uploadDocuments: "https://example.com/mashal2-docs",
+      unhcrLetter: "https://example.com/mashal2-unhcr",
+      uploadFinger: "https://example.com/mashal2-finger"
+    }
+  },
+    {
+    username: "SAYED JAMSHID",
+    password: "IT197531842",
+    ceuNumber: "64511169",
+    name: "SAYED JAMSHID",
+    lastname: "HUSSAINI",
+    nationality: "AFGHAN",
+    passportNumber: "P01550807",
+    nationalIDNumber: "1400-0101-12696",
+    gender: "male",
+    links: {
+      finalResult: "https://ambteheranesteri.github.io/approval-result-view/ITTEH-2025-5601612473-appointment.pdf",
+      uploadPassport: "https://example.com/mashal2-passport",
+      uploadDocuments: "https://example.com/mashal2-docs",
+      unhcrLetter: "https://example.com/mashal2-unhcr",
+      uploadFinger: "https://example.com/mashal2-finger"
+    }
+  },
+  {
+    username: "MASHAL SAIDY",
+    password: "IT761319690",
+    ceuNumber: "ITA/IRN/2025/761319690",
+    name: "MASHAL",
+    lastname: "SAIDY",
+    nationality: "AFGHAN",
+    passportNumber: "30811641",
+    nationalIDNumber: "30811641",
+    gender: "Female",
+    links: {
+      finalResult: "https://ambteheranesteri.github.io/approval-result-view/ITAIRN2025761319690.pdf",
+      uploadPassport: "https://example.com/farah-passport",
+      uploadDocuments: "https://example.com/farah-docs",
+      unhcrLetter: "https://example.com/farah-unhcr",
+      uploadFinger: "https://example.com/farah-finger"
+    }
+  }
 ];
-
